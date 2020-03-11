46fff7f1e752b13cfa52d394b97d53b641645c66
Nicole Yu <x1n90623@gmail.com>
2020-03-11 12:28:46 +1100

    Update app.component.ts

--- 13-pipes/app/app.component.ts
+++ 13-pipes/app/app.component.ts
@@ -19,11 +19,14 @@ interface Passenger {
             class="status"
             [class.checked-in]="passenger.checkedIn"></span>
           {{ i }}: {{ passenger.fullname }}
+//---------------------------------------------------------------------------------------
           <p>{{ passenger | json }}</p>
+//---------------------------------------------------------------------------------------
           <div class="date">
             Check in date: 
             {{ passenger.checkInDate ? (passenger.checkInDate | date: 'yMMMMd' | uppercase) : 'Not checked in' }}
           </div>
+//---------------------------------------------------------------------------------------
         </li>
       </ul>
     </div>
